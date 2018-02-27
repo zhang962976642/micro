a89b06f9759c9321389e4a6dd96933838c9bed68
zhang962976642 <962976642@qq.com>
2018-02-27 16:08:42 +0800

    更新核心组件实例化传递this问题

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,10 @@
 /** 
  * @class User
  * @description 用户模块逻辑控制器
+ * @constructor {obj} ctx -> {req: obj, res: obj, next: function, services: obj, controllers: obj}
  * @extends Controller
  * @method userHome
  *   @description 用户模块首页逻辑
- *   @param {object} ctx -> {req: obj, res: obj, next: function, services: obj}
  * @createTime 2018/02/27
  * @author zhang962976642
  * 
@@ -13,21 +13,21 @@
 const Controller = require("../lib/extends/Controller");
 
 class User extends Controller {
-  constructor(props){
-    super(props);
+  constructor(ctx){
+    super(ctx);
+    this.ctx = ctx;
   }
 
-  userHome(ctx){
-    // ctx -> {req: obj, res: obj, next: function, services: obj}
+  userHome(){
+    // ctx -> {req: obj, res: obj, next: function, services: obj, controllers: obj}
     this.validate({});
 
     // todo 需要使用多个services需实例化多个
-    let { services } = ctx,
+    let { services } = this.ctx,
       userService = new services.user(),
       data = userService.getUsers(),
       logs = userService.getLogger();
-    console.log(data);
-    console.log(logs);
+    console.log(this.method);
     return data;
   }
 
